57ce68af964da9099d6c6fa14337587f51a7904b
Sergio <sllopis@csumb.edu>
2018-09-25 19:59:44 -0700

    Update Heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/ng6-proj'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ng6-proj/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
