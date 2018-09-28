96f39ae335cfed479633878b554ba9f90cfabd8e
Sergio <sllopis@csumb.edu>
2018-09-27 20:04:01 -0700

    Update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 const express = require('express');
-const app = express.Router();
+const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist/ng6-proj'));
